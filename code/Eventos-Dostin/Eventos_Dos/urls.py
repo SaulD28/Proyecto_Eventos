4836fec3c0cdd492e8be5afcb60227f620cb08ad
SaulD28 <edidonis42@gmail.com>
2025-09-30 15:50:50 -0600

    super user y db

--- code/Eventos-Dostin/Eventos_Dos/urls.py
+++ code/Eventos-Dostin/Eventos_Dos/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('table/', views.table, name='table'),
+    path('api/eventos/', views.get_eventos, name='get_eventos'),
 
 ]
--- code/Eventos-Dostin/Eventos_Dos/views.py
+++ code/Eventos-Dostin/Eventos_Dos/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+import json
+from .models import Evento
 
-# Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
 def table(request):
     return render(request, 'table.html')
+
+def get_eventos(request):
+    eventos = evento.objects.all().values()
+    return JsonResponse(list(eventos), safe=False)
